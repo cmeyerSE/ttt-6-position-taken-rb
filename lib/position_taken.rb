a57dfb76c5c2c13dcd754ee19a29682ca0ad95ac
Cory <cameyer182@gmail.com>
2021-08-20 23:07:15 -0400

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,13 +1,10 @@
 require "pry"
 def position_taken?(board, index)
-    # board[index].nil? || board[index].empty? ? true : false
-    # board[index] != " " ? true : false
+
     if board[index] == " " || board[index] == "" || board[index] == nil 
         false
     elsif board[index] == "X" || board[index] == "O"
         true
     end
-    
-end
 
-# board[index] != " "
+end
